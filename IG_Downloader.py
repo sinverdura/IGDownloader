0b3721e62e65f54613453aa3b79673ec08960d80
sinverdura <ogomez12696@gmail.com>
2022-10-06 22:45:23 -0500

    changes did for geckodriver-v0.31.0-win64 and new laptop =)

--- IG_Downloader.py
+++ IG_Downloader.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
+#To work in new laptop
+from selenium.webdriver.common.by import By
 import time
 import getpass
 import urllib.request
 #from selenium.webdriver.common.keys import Keys
 
 list_urls=[] #se crea una lista vacia, para almacenar más tarde las URLs
-f=open('C:\\Users\\ogome\\Desktop\\Python\\WS_InstagramDownloader\\links.txt','r')
+f=open('G:\\My Drive\\WS_InstagramDownloader\\links.txt','r')
 f_lines=f.readlines()   #f_lines es la lista donde se guradan los links que 
                         #vienen en la lista
 
@@ -17,7 +19,7 @@ password_xpath='//html//body//div[1]//section//main//article//div[2]//div[1]//di
 
 password=getpass.getpass(prompt='your IGpswrd, please: ')
 number_to_save=int(input('consecutive number to your 1st video: '))
-base_ar="C:\\Users\\ogome\\Videos\\Blue Shark\\"
+base_ar="G:\\My Drive\\WS_InstagramDownloader\\downloaded_videos\\"
 for x in range(len(f_lines)): #se crea la lista de URLs
     i=number_to_save+x
     name_video=base_ar+str(i)+'.mp4'
@@ -26,24 +28,29 @@ print('Automation working...')
 
 profile = webdriver.FirefoxProfile() #for mute firefox
 profile.set_preference("media.volume_scale", "0.0") #for mute firefox
-browser=webdriver.Firefox(executable_path=r"C:\\Users\\ogome\\Desktop\\Python\\WS_InstagramDownloader\\geckodriver-v0.26.0-win64\\geckodriver.exe",firefox_profile=profile) 
-
+# browser=webdriver.Firefox(executable_path=r"G:\\My Drive\\WS_InstagramDownloader\\geckodriver-v0.26.0-win64\\geckodriver.exe", firefox_profile = profile) 
+browser=webdriver.Firefox(executable_path=r"G:\\My Drive\\WS_InstagramDownloader\\geckodriver-v0.31.0-win64\\geckodriver.exe", firefox_profile = profile) 
 browser.set_window_position(0,0)
 browser.set_window_size(1366/2,(768)) #1366 x 768 my actual desktop size 
 
 #time.sleep(2)
 browser.get("https://www.instagram.com")
 time.sleep(2)
-browser.find_element_by_xpath("//input[@name=\"username\"]").send_keys('sinverdura')
-browser.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)
-time.sleep(1)
-browser.find_element_by_xpath('//button[@type="submit"]').click()
+# browser.find_element_by_xpath("//input[@name=\"username\"]").send_keys('sinverdura')
+browser.find_element("xpath", "//input[@name=\"username\"]").send_keys('sinverdura')
+# browser.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)
+browser.find_element("xpath", "//input[@name=\"password\"]").send_keys(password)
+# browser.find_element_by_xpath('//button[@type="submit"]').click()
+browser.find_element("xpath", '//button[@type="submit"]').click()
 time.sleep(10) #incremento este valor a 10, estaba en 6 s.  
-browser.find_element_by_xpath("//button[contains(text(), 'Ahora no')]").click()
+# browser.find_element_by_xpath("//button[contains(text(), 'Ahora no')]").click()
+browser.find_element("xpath", "//button[contains(text(), 'Ahora no')]").click()
 for (a,b) in zip(f_lines,list_urls): #a==URL per vid, b==acces rute per vid
     browser.get(a)
     time.sleep(1)
-    video_url=browser.find_element_by_tag_name('video').get_attribute('src')
+    # video_url = browser.find_element_by_tag_name('video').get_attribute('src')
+    # video_url = browser.find_element('video').get_attribute('src')
+    video_url = browser.find_element(By.TAG_NAME, "video").get_attribute('src')
     browser.get(video_url)
     time.sleep(1)
     urllib.request.urlretrieve(video_url,b)
